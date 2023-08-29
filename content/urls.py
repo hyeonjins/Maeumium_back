384d1a0edef48fa97281ea257bd6b94c1d15f6fe
hyeonjins <tkrhk8011@dgu.ac.kr>
2023-08-30 01:23:16 +0900

    main 캘린더 수정

--- content/urls.py
+++ content/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import MyDiary, CoupleDiary, Main, Main2, Main3
 
 urlpatterns = [
-    path('main/', Main.as_view(), name='main2'),
+    path('main/', Main.as_view(), name='main'),
     path('main2/', Main2.as_view(), name='main2'),
     path('main3/', Main3.as_view(), name='main3'),
     path('mydiary', MyDiary.as_view(), name='mydiary'),
