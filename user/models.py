123e2b047aa224b73c814e7e6f5b49aa88c1a85b
hyeonjins <tkrhk8011@dgu.ac.kr>
2023-08-31 04:52:40 +0900

    일기 쓰기, 상세 페이지 구현

---
+++ user/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.db import models
+
+
+# Create your models here.
+class User(AbstractBaseUser):
+    """
+        유저 아이디 -> 화면 표기 이름
+        유저 이름 -> 실제 사용자 이름
+        유저 이메일 -> 회원가입시 사용하는 아이디
+        유저 비밀번호
+        유저 폰번호
+
+    """
+
+    username = None
+    id = models.CharField(unique=True, max_length=24, primary_key=True)
+    name = models.CharField(max_length=24)
+    date = models.DateField(null=True, blank=False)
+    email = models.EmailField(default="example@example.com")
+    nickname = models.CharField(unique=True, max_length=24, default="default_nickname")
+
+    USERNAME_FIELD = 'id'
+
+    def __str__(self):
+        return self.id
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        pass
+
+    class Meta:
+        db_table = "User"
